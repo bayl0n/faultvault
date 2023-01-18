0aa764d0120af312c69fc1247b9898b27f7dc403
bayl0n <nathanbaylon@gmail.com>
2023-01-18 23:54:19 +1100

    feat(faults): :sparkles: Set up faults

--- webapp/app/Models/User.php
+++ webapp/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function faults()
+    {
+        return $this->hasMany(Fault::class);
+    }
 }
